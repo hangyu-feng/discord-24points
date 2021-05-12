2a3584bf0c03e27dd2bca387fd57a6bf0b187cda
Hangyu Feng <hfeng@financeit.io>
2021-05-12 07:48:01 -0400

    remove whitespace

--- generate.js
+++ generate.js
@@ -5,7 +5,6 @@ function randInt(min, max) {
   return Math.floor(min + Math.random() * (max - min + 1));
 }
 
-
 function getGame(min = 1, max = 13) {
   const numbers = Array.from(Array(4)).map(x=>randInt(min, max));
   return numbers.join(", ");
