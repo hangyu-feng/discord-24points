f48746f8b3ade7d7de1b29da4ac8fddee23972ff
ramseyzhan <ramseyzhan@sjtu.edu.cn>
2021-05-12 14:49:25 -0700

    solve 24 points and init group at

--- generate.js
+++ generate.js
@@ -1,4 +1,9 @@
-module.exports = { getGame };
+module.exports = { getGame, getHelp, getAns, atGroup,joinGroup,leaveGroup };
+
+
+var groupMap = new Map();
+
+
 
 // return random int between min and max (including min and max)
 function randInt(min, max) {
@@ -6,6 +11,103 @@ function randInt(min, max) {
 }
 
 function getGame(min = 1, max = 13) {
-  const numbers = Array.from(Array(4)).map(x=>randInt(min, max));
-  return numbers.join(", ");
+    return Array.from(Array(4)).map(x=>randInt(min, max));
+}
+
+
+function getHelp() {
+  
+  return "\
+-getRandomGame or -getR generate four 1-13 number for the 24 points\n\
+-getGame or -getG generate four 1-13 number for the 24 points that must have answer\n\
+-join <Group> add yourself to the <Group>\n\
+-at <Group> at everyone in the <Group>\n\
+-ans get the answer of the previous four number\n\
+-help or -h get the help"
+}
+
+function atGroup(groupName) {
+  if(groupMap.has(groupName)){
+    const groupSet = groupMap.get(groupName)
+    return  Array.from(groupSet).join(" ");
+  }
+  else{
+    return "no such group";
+  }
+}
+
+function leaveGroup(groupName,auther) {
+  if(groupMap.has(groupName)){
+    let groupSet = groupMap.get(groupName);
+    groupSet.delete(auther);
+    groupMap.set(groupName,groupSet);
+  }
+  
+  return groupName;
+}
+
+function joinGroup(groupName,auther) {
+  if(!groupMap.has(groupName)){
+    let groupSet = new Set();
+    groupSet.add(auther)
+    groupMap.set(groupName,groupSet);
+  }
+  else{
+    let groupSet = groupMap.get(groupName);
+    groupSet.add(auther);
+    groupMap.set(groupName,groupSet);
+  }
+}
+
+function getAns(prevNum) {
+  let numbersStrArray = prevNum.map(x=>x.toString());
+
+  if(judge(prevNum,numbersStrArray)){
+    return res;
+  }
+  else{
+    return "no ans"
+  }
+}
+
+var res = "";
+function judge(numsArray,numsStrArray, result = 24) {
+  if (!numsArray || numsArray.length === 0) return false;
+  if (numsArray.length === 1){
+    if(Math.abs(numsArray[0] - result) < 1e-5){
+      res = numsStrArray[0];
+      return true;
+    }
+    else{
+      return false;
+    }
+  }
+
+
+  for (let i = 0; i < numsArray.length - 1; i++) {
+      for (let j = i + 1; j < numsArray.length; j++) {
+          let n1 = numsArray[i];
+          let n2 = numsArray[j];
+          let s1 = numsStrArray[i];
+          let s2 = numsStrArray[j];
+          let rest = [];
+          let restStr = [];
+
+          for (let k = 0; k < numsArray.length; k++ ){
+              if (k !== i && k !== j) {
+                  rest.push(numsArray[k]);
+                  restStr.push(numsStrArray[k]);
+              }
+          }
+          if (judge([n1 + n2, ...rest],["(" + s1 + " + " + s2 +")", ...restStr], result)
+          || judge([n1 - n2, ...rest],["(" + s1 + " - " + s2 +")", ...restStr], result)
+          || judge([n2 - n1, ...rest],["(" + s2 + " - " + s1 +")", ...restStr], result)
+          || judge([n1 * n2, ...rest],["(" + s1 + " * " + s2 +")", ...restStr], result)
+          || judge([n1 / n2, ...rest],["(" + s1 + " / " + s2 +")", ...restStr], result)
+          || judge([n2 / n1, ...rest],["(" + s2 + " / " + s1 +")", ...restStr], result)) {
+              return true;
+          }
+      }
+  }
+  return false;
 }
