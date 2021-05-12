e78ad5d7ea189a161553e77df8090e7a6d6366db
Hangyu Feng <hfeng@financeit.io>
2021-05-12 06:59:38 -0400

    update with error checking for reading confidential file

--- 24points.js
+++ 24points.js
@@ -4,7 +4,12 @@ const yaml = require('js-yaml');
 const fs = require('fs');
 
 
-const confidential = yaml.load(fs.readFileSync('confidential.yml', 'utf8'));
+const confidential = yaml.load(fs.readFileSync('confidential.yml', 'utf8', err => {
+  if (err) {
+    console.error(err)
+    return
+  }
+}));
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
