64136bd5c590ed7c19368ef8216e83b744e38ec8
Hangyu Feng <hfeng@financeit.io>
2021-05-12 07:55:24 -0400

    add missing semicolon

--- client.js
+++ client.js
@@ -1,6 +1,6 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const getGame = require('./generate').getGame
+const getGame = require('./generate').getGame;
 
 module.exports = { client };
 
--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const yaml = require('js-yaml');
 const fs = require('fs');
 
-const client = require('./client').client
+const client = require('./client').client;
 
 const confidential = yaml.load(fs.readFileSync('confidential.yml', 'utf8', err => {
   if (err) {
