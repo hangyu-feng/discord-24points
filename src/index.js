9c88d25b48760384afbffef2fe3867dfb1282533
Hangyu Feng <hfeng@financeit.io>
2021-05-12 20:06:14 -0400

    add root

--- src/index.js
+++ src/index.js
@@ -1,19 +1,18 @@
+const yaml = require('js-yaml');
+const fs = require('fs');
+const client = require('./client').client;
 const express = require('express');
 const app = express();
 const port = 3000;
+const path = require('path');
+const root = path.dirname(require.main.path);
+
 
 app.get('/', (req, res) => res.send('Hello World!'));
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
 
-
-
-const yaml = require('js-yaml');
-const fs = require('fs');
-
-const client = require('./client').client;
-
-const confidential = yaml.load(fs.readFileSync('confidential.yml', 'utf8', err => {
+const confidential = yaml.load(fs.readFileSync(path.resolve(root, './confidential.yml'), 'utf8', err => {
   if (err) {
     console.error(err)
     return
