3e673f23cb71b43a6521a644f14ef3a0799a99af
Hangyu Feng <vailgrass@gmail.com>
2021-05-17 03:33:30 -0400

    axios

--- dota.js
+++ dota.js
@@ -1,19 +1,4 @@
-const https = require('https')
-
+const axios = require('axios');
 const options = {
   hostname: 'https://api.stratz.com/',
 }
-
-const req = https.request(options, res => {
-  console.log(`statusCode: ${res.statusCode}`)
-
-  res.on('data', d => {
-    process.stdout.write(d)
-  })
-})
-
-req.on('error', error => {
-  console.error(error)
-})
-
-req.end()
