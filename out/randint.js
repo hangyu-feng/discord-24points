5b41e18caff280d3f7a1930244be80e5ccf7975c
Hangyu Feng <hfeng@financeit.io>
2021-05-19 20:02:27 -0400

    rm compiled js files

--- out/randint.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.randInt = void 0;
-function randInt(min, max) {
-    // inclusive
-    return Math.floor(min + Math.random() * (max - min + 1));
-}
-exports.randInt = randInt;
-//# sourceMappingURL=randint.js.map
